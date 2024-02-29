eb58a0f88ab3c51c49dde46a06aa8edbd1e18df2
Andres Cabezas <131211076+JesusAndres2512@users.noreply.github.com>
2024-02-29 11:57:11 -0500

    Layouts

---
+++ app/src/main/java/moviles/apphoteles/MainActivity.java
@@ -0,0 +1,13 @@
+package moviles.apphoteles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
